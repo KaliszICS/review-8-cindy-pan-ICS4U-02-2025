15f6c71638643cf6e047ea1546922819eb6ef6c5
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-02-18 15:20:54 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,47 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+
+	public static String pluralize(String word)
+	{
+		String end = "y";
+		String endAgain = "ey";
+		String endLast = "ife";
+		if (word.substring(word.length()).equalsIgnoreCase(end)){
+			return "ies";
+		}
+		else if (word.substring((word.length()-3)).equalsIgnoreCase(endAgain)){
+			return "eys";
+		}
+		else if (word.substring(word.length()-3).equalsIgnoreCase(endLast)){
+			return "ives";
+		}
+		else {
+			return "s";
+		}
 
 	}
 
+	public static int min(int num, int numagain, int numlast){
+		if (num < numagain && num < numlast){
+			return num;
+		}
+		else if (numagain<num && numagain<numlast){
+			return numagain;
+		}
+		else{
+			return numlast;
+		}
+		
+
+	public static String isLeapYear(int year){
+		if (year%4 == 0){
+			
+		}
+	}
+
+	}
+
+
+
+	//public static 
 }
